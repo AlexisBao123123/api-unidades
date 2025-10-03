d4b76ced9ce65e46f352c32179ecc830faa3ee74
AlexisBao123123 <alexisbao5050@gmail.com>
2025-10-03 10:25:09 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());                  // habilita peticiones externas
+app.use(express.json());          // <--- necesario para leer JSON
+app.use(express.urlencoded({ extended: true })); // para leer formularios
+
+const unidadesRoutes = require('./routes/unidades.js');
+app.use('/api/unidades', unidadesRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
